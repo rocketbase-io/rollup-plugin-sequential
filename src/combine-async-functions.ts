1ccf09c5bbca6fc39d62498d99f6510f69b17e8b
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2020-02-18 14:15:22 +0100

    feat: preserve plugin context

--- src/combine-async-functions.ts
+++ src/combine-async-functions.ts
@@ -1,10 +1,10 @@
 export function combineAsyncFunctions(name: string, plugins: any[], once: boolean) {
   let ranBefore = false;
-  return (...params: any[]) => {
+  return function(this: any, ...params: any[]) {
     if (once && ranBefore) return;
     ranBefore = true;
     return plugins.reduce((before, plugin) => {
-      return before.then(() => plugin[name].call(plugin, ...params));
+      return before.then(() => plugin[name].call(this, ...params));
     }, Promise.resolve());
   };
 }
